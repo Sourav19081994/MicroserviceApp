9d76f0a6b6db064e46677812fc829296e5eda8cc
Sourav Majumdar <mazumdar.19081994@gmail.com>
2021-08-25 22:03:19 +0530

    Repository added

---
+++ MicroserviceApp/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,72 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProduct());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProduct()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "IPhone X",
+                    Summary = "asd",
+                    Description = "asd",
+                    ImageFile = "product1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "IPhone X",
+                    Summary = "asd",
+                    Description = "asd",
+                    ImageFile = "product1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "IPhone X",
+                    Summary = "asd",
+                    Description = "asd",
+                    ImageFile = "product1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "IPhone X",
+                    Summary = "asd",
+                    Description = "asd",
+                    ImageFile = "product1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "IPhone X",
+                    Summary = "asd",
+                    Description = "asd",
+                    ImageFile = "product1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                }
+            };
+        }
+    }
+}
