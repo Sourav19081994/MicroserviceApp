65fc9ebfd4541ee556f51d82a8cf908874365608
Sourav Majumdar <mazumdar.19081994@gmail.com>
2021-08-26 21:45:30 +0530

    docker-compose.yml added

--- MicroserviceApp/Catalog.API/Controllers/CatalogController.cs
+++ MicroserviceApp/Catalog.API/Controllers/CatalogController.cs
@@ -55,9 +55,9 @@ namespace Catalog.API.Controllers
         [Route("[action]/{category}")]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string categoryName)
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
         {
-            var products = await _repository.GetProductByCategory(categoryName);
+            var products = await _repository.GetProductByCategory(category);
             return Ok(products);
         }
 
