9e6078b5b4c248517be05437eb5e188294aaabe4
Sourav Majumdar <mazumdar.19081994@gmail.com>
2021-09-27 22:26:12 +0530

    Web app added

---
+++ MicroserviceApp/src/WebApp/AspnetRunBasics/ApiCollection/Infrastructure/BaseHttpClientWithFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Infrastructure
+{
+    public abstract class BaseHttpClientWithFactory
+    {
+        private readonly IHttpClientFactory _factory;
+        public Uri BaseAddress { get; set; }
+        public string BasePath { get; set; }
+
+        public BaseHttpClientWithFactory(IHttpClientFactory factory) => _factory = factory;
+    }
+}
